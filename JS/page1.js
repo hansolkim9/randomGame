2e7f85a4fe92a2790ae46208366c8faf2c143a4a
kimeunji <eunji93121@gmail.com>
2024-04-10 18:19:15 +0900

    css 그림자 추가  및 각 페이지 링크 연결

--- JS/page1.js
+++ JS/page1.js
@@ -1,11 +1,7 @@
-
-
 document.addEventListener('DOMContentLoaded', function() {
-  const button = document.querySelector('.intro__button.show');
-  button.addEventListener('click', function() {
-      
-      const documentHeight = document.body.scrollHeight;
-    
+  const button = document.querySelector('.intro__button.show'); 
+  button.addEventListener('click', function() {     
+      const documentHeight = document.body.scrollHeight;    
       window.scrollTo({
           top: documentHeight,
           behavior: 'smooth'
@@ -15,39 +11,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   const title = document.querySelector('.intro__title');
-  const text = title.textContent; // 텍스트 내용 가져오기
-  title.innerHTML = ''; // 텍스트 내용 초기화
+  const text = title.textContent; 
+  title.innerHTML = '';
 
-  // 각 글자를 순차적으로 나타내기 위한 반복문
   for (let i = 0; i < text.length; i++) {
       setTimeout(function() {
-          // 각 글자를 title에 추가하여 순차적으로 나타내기
+        
           title.innerHTML += text[i];
-      }, i * 100); // 각 글자마다 0.1초씩 지연 적용 (원하는 딜레이 조절 가능)
+      }, i * 100); // 각 글자마다 0.1초씩 지연 적용
   }
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  var title = document.querySelector('.intro__title');
-  var text = title.innerHTML;
-  title.innerHTML = ''; // 텍스트 초기화
-
-  // 한 글자씩 출력하기 위한 함수
-  function typeWriter(text, i) {
-      if (i < text.length) {
-          title.innerHTML += text.charAt(i);
-          i++;
-          setTimeout(function() {
-              typeWriter(text, i);
-          }, 100); // 100밀리초마다 한 글자씩 출력
-      } else {
-          // 모든 텍스트 출력 후에 커서 이동
-          title.style.borderRight = 'none'; // 커서 제거
-      }
-  }
-
-  // 시작
-  typeWriter(text, 0);
 });
 
 
