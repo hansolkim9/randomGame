bc21d36c14cfa587d47b9a8b80c8c143a4f8258a
hansolkim9 <masicer2@gmail.com>
2024-04-10 14:53:58 +0900

    # clickRandomCom.js 14-15, 29-32, 39-41 - turn 변경 적용 (PC - YOU) - YOU로 변경 시 애니메이션 적용 클래스 추가, PC로 변경시 애니메이션 클래스 삭제

--- JS/clickRandomCom.js
+++ JS/clickRandomCom.js
@@ -11,6 +11,8 @@ const $selectOrder = document.querySelector(".menu-nav-turn > p");
 const executingValueObject = {
   isExecuting: false,
 };
+// turn 변경을 위한 변수 지정
+const $turn = document.querySelector('.game-turn');
 const execute = () => {
   if (document.querySelector(".stage-layout").classList.contains("stageshow")) {
     document.querySelector(".stage-layout").classList.remove("stageshow");
@@ -24,12 +26,19 @@ const execute = () => {
       box.classList.remove(`bgColorChange`);
     }
   });
+  // turn : PC로 변경, 애니메이션 제거
+  $turn.textContent = 'PC';
+  $turn.classList.remove('blinking-text');
+
   let index = 0; // 초기 인덱스 설정
   const intervalId = setInterval(() => {
     if (index >= randomArray.length) {
       clearInterval(intervalId); // 배열의 끝에 도달하면 인터벌 정지 //끝나면 다시 ㄴㄴ로 바꿈
 
       // console.log(index);
+      // turn : YOU로 변경, blink 애니매이션(클래스로) 추가
+      $turn.textContent = 'YOU';
+      $turn.classList.add('blinking-text');
       setTimeout(() => {
         clickBox();
       }, 400);
